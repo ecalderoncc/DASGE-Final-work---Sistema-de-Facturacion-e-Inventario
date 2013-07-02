c973470ead66a580a1d8d521e4656565fc3b2242
Eduardo de Rivero Manrique <ederiveoman@gmail.com>
2013-07-02 17:41:46 -0500

    Agregue el Mecanismo de Espera en el Login Pues demora al rededor de 5seg.
    =)

--- ProyectoOmegaR_28-05-2013_MySQLenLinea/ProyectoOmegaR/Login.cs
+++ ProyectoOmegaR_28-05-2013_MySQLenLinea/ProyectoOmegaR/Login.cs
@@ -33,6 +33,8 @@ namespace ProyectoOmegaR
             }
             if (f == 0)
             {
+                
+
                 string observer = Link_BD.Login(textBnombre.Text, txtpass.Text);
                 switch (observer)
                 {
@@ -48,6 +50,18 @@ namespace ProyectoOmegaR
                         }
                     case "OK":
                         {
+                            //--MECANISMO DE ESPERA--
+                            BarraProgreso.Maximum = 1000000;
+                            BarraProgreso.Minimum = 0;
+                            BarraProgreso.Value = 0;
+                            BarraProgreso.Step = 1;
+                            for (int i = BarraProgreso.Minimum; i < BarraProgreso.Maximum; i = i + BarraProgreso.Step)
+                            {
+                                //esta instrucción avanza la posición actual de la barra
+                                BarraProgreso.PerformStep();
+                            }
+                            //--TERMINO DE MECANISMO DE ESPERA--
+
                             HomeOmega A = new HomeOmega(Link_BD.get_CampoUsu("UsuNom"),this);
                             A.Show();
                             txtpass.Text = "";
